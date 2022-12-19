d7a8f970cd4e16d492b95eb5f4d673d14f06b6e1
JaredPhelps <jared@inksoft.com>
2022-12-18 21:46:44 -0700

    Day 16 - pretty suboptimal

---
+++ AdventOfCode/Days/Day16Part1.cs
@@ -0,0 +1,123 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+using System.Text;
+
+namespace AdventOfCode.Days
+{
+    public class Day16Part1
+    {
+        public int Solve(string input, int minutesLeft)
+        {
+            Dictionary<string, Node> nodes = ParseInput(input);
+            int i = 0;
+            foreach (var n in nodes)
+            {
+                nodeIndexes.Add(n.Value.Name, 1L << i++);
+            }
+
+            Node startingNode = nodes["AA"];
+            int score = startingNode.OptimalPath(minutesLeft, new HashSet<string>());
+            return score;
+        }
+
+        private Dictionary<string, Node> ParseInput(string input)
+        {
+            var nodes = new Dictionary<string, Node>();
+            string[] lines = input.Split("\r\n");
+
+            // Just get a dictionary of all the nodes for now.
+            foreach (var line in lines)
+            {
+                string s = new string(line.Skip(1).Where(c => char.IsUpper(c)).ToArray());
+                for (int i = 0; i < s.Length; i+=2)
+                {
+                    string nodeName = s.Substring(i, 2);
+                    if (!nodes.ContainsKey(nodeName))
+                        nodes.Add(nodeName, new Node(nodeName));
+                }
+            }
+
+            // fill in flow rate and children
+            foreach (var line in lines)
+            {
+                string s = new string(line.Skip(1).Where(c => char.IsUpper(c)).ToArray());
+                Node n = nodes[s.Substring(0, 2)];
+                n.FlowRate = int.Parse(line.Split('=', ';')[1]);
+                for (int i = 2; i < s.Length; i += 2)
+                {
+                    string nodeName = s.Substring(i, 2);
+                    n.Children.Add(nodes[nodeName]);
+                }
+            }
+
+            return nodes;
+        }
+
+
+        private static Dictionary<string, long> nodeIndexes = new Dictionary<string, long>();
+        private static Dictionary<(string, int, long), int> cache = new Dictionary<(string, int, long), int>();
+
+        private class Node
+        {
+            public List<Node> Children = new List<Node>();
+            public string Name { get; set; }
+            public int FlowRate { get; set; }
+            public Node(string name)
+            {
+                Name = name;
+            }
+
+            public int OptimalPath(int minutesLeft, HashSet<string> openValves)
+            {
+                if (minutesLeft <= 1)
+                    return 0;
+
+                int? cachedValue = OptimalPathCache(minutesLeft, openValves);
+                if (cachedValue != null)
+                    return cachedValue.Value;
+
+                long openValveSetHash = OpenValveSetHash(openValves);
+
+
+                // I can spend a minute opening my current valve then going to each of my children, or I can spend a minute going to each of my children.
+                // But if my valve is open already, all I can do is travel to my children again.
+
+                int openScore = 0;
+                // Even if we were here before, we might want to open it now instead of earlier, because we might have been chasing a very high value valve.
+                if (!openValves.Contains(Name) && FlowRate > 0)
+                {
+                    openValves.Add(Name);
+                    openScore = (FlowRate * (minutesLeft - 1)) + Children.Max(c => c.OptimalPath(minutesLeft - 2, openValves));
+                    openValves.Remove(Name);
+                }
+
+                int skipThisValveScore = Children.Max(c => c.OptimalPath(minutesLeft - 1, openValves));
+
+                cache.Add((Name, minutesLeft, openValveSetHash), Math.Max(skipThisValveScore, openScore));
+                return Math.Max(skipThisValveScore, openScore);
+            }
+
+            // We can cache values that indicate that you're at this node with this many minutes left and this set of open valves.
+            public int? OptimalPathCache(int minutesLeft, HashSet<string> openValves)
+            {
+                long openValvesHash = OpenValveSetHash(openValves);
+                if (cache.ContainsKey((Name, minutesLeft, openValvesHash)))
+                    return cache[(Name, minutesLeft, openValvesHash)];
+
+                return default;
+            }
+
+            public long OpenValveSetHash(HashSet<string> openValves)
+            {
+                long openValvesHash = 0;
+                foreach (string v in openValves)
+                {
+                    openValvesHash ^= nodeIndexes[v];
+                }
+                return openValvesHash;
+            }
+        }
+    }
+
+}
+
---
+++ AdventOfCode/Days/Day16Part2.cs
@@ -0,0 +1,236 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+using System.Text;
+using System.Xml.Linq;
+
+namespace AdventOfCode.Days
+{
+    public class Day16Part2
+    {
+        public int Solve(string input, int minutesLeft)
+        {
+            Dictionary<string, Node> nodes = ParseInput(input);
+            int i = 0;
+            foreach (var n in nodes)
+            {
+                nodeIndexes.Add(n.Value.Name, 1L << i++);
+            }
+
+            NodePair startingNode = allNodePairs["AA"]["AA"];
+            int score = startingNode.OptimalPath(minutesLeft, new HashSet<string>());
+            return score;
+        }
+
+        private Dictionary<string, Node> ParseInput(string input)
+        {
+            string[] lines = input.Split("\r\n");
+            List<string> nodeNames = lines.Select(l => l.Substring("Valve ".Length, 2)).ToList();
+
+            var nodes = nodeNames.ToDictionary(nn => nn, nn => new Node(nn));
+
+
+            // fill in flow rate and children
+            foreach (var line in lines)
+            {
+                string s = new string(line.Skip(1).Where(c => char.IsUpper(c)).ToArray());
+                Node n = nodes[s.Substring(0, 2)];
+                n.FlowRate = int.Parse(line.Split('=', ';')[1]);
+                for (int i = 2; i < s.Length; i += 2)
+                {
+                    string nodeName = s.Substring(i, 2);
+                    n.Children.Add(nodes[nodeName]);
+                }
+            }
+
+            for (int i = 0; i < nodeNames.Count; i++)
+            {
+                Node n1 = nodes[nodeNames[i]];
+                
+                for (int j = i; j < nodeNames.Count; j++)
+                {
+                    Node n2 = nodes[nodeNames[j]];
+                    string key1 = n1.Name.CompareTo(n2.Name) < 0 ? n1.Name : n2.Name;
+                    string key2 = n1.Name.CompareTo(n2.Name) < 0 ? n2.Name : n1.Name;
+                    if (!allNodePairs.ContainsKey(key1))
+                        allNodePairs.Add(key1, new Dictionary<string, NodePair>());
+
+                    allNodePairs[key1].Add(key2, new NodePair(key1.CompareTo(key2) < 0 ? n1 : n2, key1.CompareTo(key2) < 0 ? n2 : n1));
+                }
+            }
+
+            return nodes;
+        }
+
+        private static long cacheHits = 0;
+        private static long cacheMisses = 0;
+        private static Dictionary<string, long> nodeIndexes = new Dictionary<string, long>();
+        private static Dictionary<(string, string, int, long), int> cache = new Dictionary<(string, string, int, long), int>();
+
+        private static Dictionary<string, Dictionary<string, NodePair>> allNodePairs = new Dictionary<string, Dictionary<string, NodePair>>();
+
+        private class NodePair
+        {
+            public Node n1;
+            public Node n2;
+
+            public NodePair(Node n1, Node n2)
+            {
+                this.n1 = n1;
+                this.n2 = n2;
+
+                if (n1.Name.CompareTo(n2.Name) < 0)
+                {
+                    this.n1 = n2;
+                    this.n2 = n1;
+                }
+            }
+
+            public IEnumerable<NodePair> BothStayChildren => BothLeaveChildren;
+            public IEnumerable<NodePair> BothLeaveChildren
+            {
+                get
+                {
+                    foreach (var n1Child in n1.Children)
+                    {
+                        foreach (var n2Child in n2.Children)
+                        {
+                            Node n1 = n1Child.Name.CompareTo(n2Child.Name) < 0 ? n1Child : n2Child;
+                            Node n2 = n1Child.Name.CompareTo(n2Child.Name) < 0 ? n2Child : n1Child;
+                            yield return allNodePairs[n1.Name][n2.Name];
+                        }
+                    }
+                }
+            }
+
+            public IEnumerable<NodePair> N1LeavesChildren
+            {
+                get
+                {
+                    foreach (var n1Child in n1.Children)
+                    {
+                        Node n1 = n1Child.Name.CompareTo(this.n2.Name) < 0 ? n1Child : this.n2;
+                        Node n2 = n1Child.Name.CompareTo(this.n2.Name) < 0 ? this.n2 : n1Child;
+                        yield return allNodePairs[n1.Name][n2.Name];
+                    }
+                }
+            }
+
+            public IEnumerable<NodePair> N2LeavesChildren
+            {
+                get
+                {
+                    foreach (var n2Child in n2.Children)
+                    {
+                        Node n1 = n2Child.Name.CompareTo(this.n1.Name) < 0 ? n2Child : this.n1;
+                        Node n2 = n2Child.Name.CompareTo(this.n1.Name) < 0 ? this.n1 : n2Child;
+                        yield return allNodePairs[n1.Name][n2.Name];
+                    }
+                }
+            }
+
+            public int OptimalPath(int minutesLeft, HashSet<string> openValves)
+            {
+                if (minutesLeft <= 1)
+                    return 0;
+
+                int? cachedValue = OptimalPathCache(minutesLeft, openValves);
+                if (cachedValue != null)
+                {
+                    cacheHits++;
+                    return cachedValue.Value;
+                }
+                cacheMisses++;
+
+                if ((cacheHits + cacheMisses) % 100000 == 0)
+                {
+                    Debug.WriteLine(((decimal)cacheHits / (cacheHits + cacheMisses) * 100M).ToString("F2"));
+                }
+
+                long openValveSetHash = OpenValveSetHash(openValves);
+
+
+                int bothOpenScore = 0;
+                int n1OpenScore = 0;
+                int n2OpenScore = 0;
+                int neitherOpenScore = 0;
+
+                // I can spend a minute opening my current valve then going to each of my children, or I can spend a minute going to each of my children.
+                // But if my valve is open already, all I can do is travel to my children again.
+
+                // We can both turn our valves
+                if (n1.Name != n2.Name && !openValves.Contains(n1.Name) && !openValves.Contains(n2.Name) && n1.FlowRate > 0 && n2.FlowRate > 0)
+                {
+                    openValves.Add(n1.Name);
+                    openValves.Add(n2.Name);
+                    bothOpenScore = (n1.FlowRate + n2.FlowRate) * (minutesLeft - 1) + BothStayChildren.Max(c => c.OptimalPath(minutesLeft - 2, openValves));
+                    openValves.Remove(n1.Name);
+                    openValves.Remove(n2.Name);
+                }
+
+                // n1 turns their valve, n2 leaves.
+                if (!openValves.Contains(n1.Name) && n1.FlowRate > 0)
+                {
+                    openValves.Add(n1.Name);
+                    n1OpenScore = n1.FlowRate * (minutesLeft - 1) + N2LeavesChildren.Max(c => c.OptimalPath(minutesLeft - 1, openValves));
+                    openValves.Remove(n1.Name);
+                }
+
+                // n2 turns their valve, n1 leaves.
+                if (!openValves.Contains(n2.Name) && n2.FlowRate > 0)
+                {
+                    openValves.Add(n2.Name);
+                    n2OpenScore = n2.FlowRate * (minutesLeft - 1) + N1LeavesChildren.Max(c => c.OptimalPath(minutesLeft - 1, openValves));
+                    openValves.Remove(n2.Name);
+                }
+
+                // both leave
+                if (true)
+                {
+                    neitherOpenScore = BothLeaveChildren.Max(c => c.OptimalPath(minutesLeft - 1, openValves));
+                }
+
+                string key1 = n1.Name.CompareTo(n2.Name) < 0 ? n1.Name : n2.Name;
+                string key2 = n1.Name.CompareTo(n2.Name) < 0 ? n2.Name : n1.Name;
+                int score = new[] { bothOpenScore, n1OpenScore, n2OpenScore, neitherOpenScore }.Max();
+                cache.Add((key1, key2, minutesLeft, openValveSetHash), score);
+                return score;
+            }
+
+            // We can cache values that indicate that you're at this node with this many minutes left and this set of open valves.
+            public int? OptimalPathCache(int minutesLeft, HashSet<string> openValves)
+            {
+                string key1 = n1.Name.CompareTo(n2.Name) < 0 ? n1.Name : n2.Name;
+                string key2 = n1.Name.CompareTo(n2.Name) < 0 ? n2.Name : n1.Name;
+
+                long openValvesHash = OpenValveSetHash(openValves);
+                if (cache.ContainsKey((key1, key2, minutesLeft, openValvesHash)))
+                    return cache[(key1, key2, minutesLeft, openValvesHash)];
+
+                return default;
+            }
+
+            public long OpenValveSetHash(HashSet<string> openValves)
+            {
+                long openValvesHash = 0;
+                foreach (string v in openValves)
+                {
+                    openValvesHash ^= nodeIndexes[v];
+                }
+                return openValvesHash;
+            }
+        }
+
+        private class Node
+        {
+            public List<Node> Children = new List<Node>();
+            public string Name { get; set; }
+            public int FlowRate { get; set; }
+            public Node(string name)
+            {
+                Name = name;
+            }
+        }
+    }
+
+}
+
